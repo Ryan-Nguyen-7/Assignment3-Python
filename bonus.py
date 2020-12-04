a0d75b67d9689a277b6a6f9725a2435111954ed2
Ubuntu <ubuntu@ip-172-31-40-214.ec2.internal>
2020-12-04 23:20:10 +0000

    added bonus.py program and constants

--- bonus.py
+++ bonus.py
@@ -4,22 +4,24 @@
 # Created on December 2020
 # This program calculates an employee's bonus
 
-
 import constants
+
+
 def main():
     # input
     current_salary = int(input("Enter your current salary : "))
     years_of_service = int(input("Enter your number of years of service : "))
     print("")
-    
+
     # process
     bonus = current_salary*constants.BONUS_PERCENTAGE
     salary_with_bonus = current_salary + bonus
-    
-    #output
+
+    # output
     if years_of_service > constants.REQUIREMENT:
         print("Your bonus is ${}".format(bonus))
-        print("Your new salary, including bonus is ${}".format(salary_with_bonus))
+        print("Your new salary, including bonus is ${}".format
+              (salary_with_bonus))
     elif years_of_service < constants.REQUIREMENT:
         print("You need more than 5 years of service to gain a bonus:(")
     else:
